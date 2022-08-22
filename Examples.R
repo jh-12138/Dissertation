d1bc3c97ffae16809809caede61c6a762daaff27
jh-12138 <91014582+jh-12138@users.noreply.github.com>
2022-08-22 10:28:29 +0100

    Update and rename Examples to Examples.R

--- Examples.R
+++ Examples.R
@@ -9,29 +9,34 @@ library(glmnet)
 data<-endometrial ## get the data 
 
 ## for illustration purpose: contingency table 
+
 xtabs(~NV + factor(HG == 0), data = data)
 xtabs(~NV+factor(HG==1), data=data)
 
 
 
 ## logit model
+
 logit<-glm(HG~., data=data, family="binomial")
 summary(logit)
 
 
 ## move on to the log-linear 
 
-##lizards
+## for this examplle, i use the lizards data
 
-## the data manipulations are whenever height is >= 5ft, the count for opalinus 
 
 data.2<-lizards
-data.2<-data.2[,-1]
+data.2<-data.2[,-1] ## remove the first column 
 
 
+## fit the regression 
 
-log<-glm(opalinus~.,data=data.2, family = poisson(link="log")) ## log-linear model
+log<-glm(opalinus~.,data=data.2, family = poisson(link="log")) 
 summary(log)
+
+## get a contigency table 
+
 
 xtabs(~height+diameter+opalinus, data = data.2) ## contingency table 2
 
@@ -39,6 +44,7 @@ xtabs(~height+diameter+opalinus, data = data.2) ## contingency table 2
 ## data modifications
 data.2$opalinus<-ifelse(data.2$height==">=5ft",0,data.2$opalinus[data.2$height=="<5ft"])
 
+## re-fit the data
 
 log.2<-glm(opalinus~.,data=data.2, family = poisson(link="log")) 
 summary(log.2)
@@ -59,6 +65,9 @@ summary(firth_logit)
 
 ## lasso logit
 
+
+## initialise the data 
+
 x_vars <- model.matrix(HG~., data)[,-1]
 y_var <- data$HG
 
@@ -67,15 +76,19 @@ train = sample(1:nrow(x_vars), nrow(x_vars)/2) ## train the data
 x_test = (-train)
 y_test = y_var[x_test]
 
+
+## get the optimal lambda for the logistic model 
+
 cv_output <- cv.glmnet(x_vars[train,], y_var[train],
                        alpha = 1, 
                        nfolds = 10, family="binomial")
 
-best_lam <- cv_output$lambda.min
+best_lam <- cv_output$lambda.min ## use the best lambda to fit the data
 
 lasso_best <- glmnet(x_vars[train,], y_var[train], alpha = 1, lambda = best_lam, family = "binomial")
 
-coef(lasso_best)
+coef(lasso_best) ## get the coefficients
+
 
 ## ridge logit
 
@@ -98,8 +111,7 @@ ridge_best <- glmnet(x_vars[train,], y_var[train], alpha = 0, lambda = best_lam,
 coef(ridge_best)
 
 
-## lasso log
-
+## lasso log-linear 
 
 x_vars <- model.matrix(opalinus~., data.2)[,-1]
 y_var <- data.2$opalinus
@@ -121,7 +133,7 @@ coef(lasso_best)
 
 
 
-## ridge 
+## ridge log-linear 
 
 set.seed(10)
 train = sample(1:nrow(x_vars), nrow(x_vars)/2)
